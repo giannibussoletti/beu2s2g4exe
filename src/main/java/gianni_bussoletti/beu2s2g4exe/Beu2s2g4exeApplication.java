2b68079f4bc149153a2b8c527c83f90db841b534
Gianni Bussoletti <giannibussoletti@gmail.com>
2026-07-09 13:45:36 +0200

    Initial commit

---
+++ src/main/java/gianni_bussoletti/beu2s2g4exe/Beu2s2g4exeApplication.java
@@ -0,0 +1,13 @@
+package gianni_bussoletti.beu2s2g4exe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Beu2s2g4exeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Beu2s2g4exeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gianni_bussoletti/beu2s2g4exe/Beu2s2g4exeApplicationTests.java
@@ -0,0 +1,13 @@
+package gianni_bussoletti.beu2s2g4exe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Beu2s2g4exeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
